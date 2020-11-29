5e3659221ca859fe5fdca44f0e643fa74bedc6d7
Mohamed Basheer <mymbasheer@hotmail.com>
2020-11-29 17:10:23 +0530

    Add project files.

---
+++ Model/ModelProduct/IProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.ModelProduct
+{
+    public interface IProduct
+    {
+        void AddProduct(string productId, string productName, decimal productCost, decimal productPrice);
+        void SearchProduct(string productId);
+        void UpdateProduct(string productId, string productName, decimal productCost, decimal productPrice);
+        void DeleteProduct(string productId);
+        void ListProduct();
+    }
+}
---
+++ Services/ServiceProduct/ProductServices.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model.ModelProduct;
+
+namespace Services.ServiceProduct
+{
+    public class ProductServices : IProduct
+    {
+        // Created Product List 
+        List<Product> products;
+
+
+        public ProductServices()
+        {
+            products = new List<Product>(); //Intantiation of Product List
+        }
+
+        public void AddProduct(string productId, string productName, decimal productCost, decimal productPrice)
+        {
+            try
+            {
+                Product product = new Product()
+                {
+                    ProductId = productId,
+                    ProductName = productName,
+                    ProductCost = productCost,
+                    ProductPrice = productPrice
+                };
+
+                products.Add(product);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void DeleteProduct(string productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ListProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SearchProduct(string productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(string productId, string productName, decimal productCost, decimal productPrice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
